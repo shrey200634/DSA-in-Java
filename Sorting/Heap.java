833276ac69b782f06f6859c51d5c8a9601f80e64
shrey200634 <dshrey60@gmail.com>
2026-02-12 23:24:13 +0530

    add heap data structure and heap sort

---
+++ Sorting/Heap.java
@@ -0,0 +1,7 @@
+package Sorting;
+
+// it is avaible in heap folder 
+// geap sort is nothing ...its just whn u remove item from the tree jut collect it in arrayListt ,
+//or vector list .... thast is basically the heap sort ;
+    
+
