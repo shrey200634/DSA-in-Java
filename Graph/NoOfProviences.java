2b0331a2e5102c0e59a3ab4b0a75fbcbd98ee639
shrey200634 <dshrey60@gmail.com>
2026-03-03 23:09:58 +0530

    add aprovience concept

---
+++ Graph/NoOfProviences.java
@@ -0,0 +1,29 @@
+package Graph;
+
+public class NoOfProviences {
+      public int findCircleNum(int[][] isConnected) {
+        int n = isConnected.length ;
+        boolean [] visited = new boolean[n];
+        int count =0;
+
+
+        for (int i =0 ; i < n ; i++){
+            if (!visited[i]){
+                count ++;
+                dfs(isConnected, visited , i);// visited all 
+            }
+        }
+        return count ;
+        
+        
+    }
+    private void dfs(int [][]isConnected , boolean [] visited , int i ){
+        for (int j =0 ; j< isConnected.length;j++){
+            if (isConnected[i][j]==1 && !visited[j]){
+                visited[j]=true ;
+                dfs(isConnected, visited ,j);
+            }
+        }
+    }
+    
+}
