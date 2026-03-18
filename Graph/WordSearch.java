e1076b9a1e67f76a426b06c47627621a23c0143b
shrey200634 <dshrey60@gmail.com>
2026-03-19 00:54:18 +0530

    add new topics on graph

---
+++ Graph/WordSearch.java
@@ -0,0 +1,47 @@
+package Graph;
+
+public class WordSearch {
+
+
+
+    public boolean exist(char[][] board, String word) {
+        int rows = board.length;
+        int cols = board[0].length;
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                 if (board[i][j] == word.charAt(0) && backtrack(board, word, i, j, 0)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private boolean backtrack(char[][] board, String word, int r, int c, int index) {
+         if (index == word.length()) {
+            return true;
+        }
+
+         if (r < 0 || r >= board.length || c < 0 || c >= board[0].length || board[r][c] != word.charAt(index)) {
+            return false;
+        }
+
+         char temp = board[r][c];
+        board[r][c] = '#';
+
+         boolean found = backtrack(board, word, r + 1, c, index + 1) ||
+                        backtrack(board, word, r - 1, c, index + 1) ||
+                        backtrack(board, word, r, c + 1, index + 1) ||
+                        backtrack(board, word, r, c - 1, index + 1);
+
+         board[r][c] = temp;
+
+        return found;
+    }
+}
+
+
+
+    
+
