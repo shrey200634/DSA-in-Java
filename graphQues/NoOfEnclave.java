c52cd971df6d3378918978d3aacaeabea2762a53
shrey200634 <dshrey60@gmail.com>
2026-03-22 00:49:57 +0530

    add new no of enclave ques

---
+++ graphQues/NoOfEnclave.java
@@ -0,0 +1,64 @@
+package graphQues;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+class Pair {
+    int first;
+    int second;
+
+    public Pair(int first, int second) {
+        this.first = first;
+        this.second = second;
+    }
+}
+
+class Solution {
+    public int numEnclaves(int[][] grid) {
+        Queue<Pair> queue = new LinkedList<>(); 
+        int n = grid.length;
+        int m = grid[0].length;
+
+        int[][] visited = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (i == 0 || j == 0 || i == n - 1 || j == m - 1) {
+                    if (grid[i][j] == 1) {
+                        queue.add(new Pair(i, j));
+                        visited[i][j] = 1;
+                    }
+                }
+            }
+        }
+
+        int[] delRow = {-1, 0, 1, 0};
+        int[] delCol = {0, 1, 0, -1};
+
+        while (!queue.isEmpty()) {
+            Pair curr = queue.poll(); 
+            int row = curr.first;
+            int col = curr.second;
+
+            for (int i = 0; i < 4; i++) {
+                int nrow = row + delRow[i];
+                int ncol = col + delCol[i];
+
+                if (nrow >= 0 && nrow < n && ncol >= 0 && ncol < m &&
+                    visited[nrow][ncol] == 0 && grid[nrow][ncol] == 1) {
+                    
+                    queue.add(new Pair(nrow, ncol));
+                    visited[nrow][ncol] = 1;
+                }
+            }
+        }
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == 1 && visited[i][j] == 0) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
