6ee2ebfbff9d9461b764e3155a4aea6aa4ab7d16
shrey200634 <dshrey60@gmail.com>
2026-07-23 23:38:27 +0530

    add the new dp memo sol

--- DynamicProgramming/TargetSumWays.java
+++ DynamicProgramming/TargetSumWays.java
@@ -1,27 +1,59 @@
 package DynamicProgramming;
 
+import java.util.Arrays;
+
 public class TargetSumWays {
     public static int findWays(int num[], int tar) {
         int n = num.length;
-        // dp[j] will store the number of subsets with sum equal to j
         int[] dp = new int[tar + 1];
         
-        // Base case: There is 1 way to make sum 0 (by choosing an empty subset)
         dp[0] = 1;
         
-        // Initialize for the first element if it does not exceed target
         if (num[0] <= tar) {
             dp[num[0]] = 1;
         }
         
-        // Fill the DP table
         for (int i = 1; i < n; i++) {
-            // Traverse backwards to avoid overwriting values needed for the current row
             for (int j = tar; j >= num[i]; j--) {
                 dp[j] = dp[j] + dp[j - num[i]];
             }
         }
         
         return dp[tar];
+    }
+
+
+
+
+    
+    private static int solve(int idx, int target, int[] num, int[][] dp) {
+        if (idx == 0) {
+            if (target == 0 && num[0] == 0) return 2;
+            
+            if (target == 0 || num[0] == target) return 1;
+            
+            return 0; 
+        }
+        
+        if (dp[idx][target] != -1) {
+            return dp[idx][target];
+        }
+            int notPick = solve(idx - 1, target, num, dp);
+        
+        int pick = 0;
+        if (num[idx] <= target) {
+            pick = solve(idx - 1, target - num[idx], num, dp);
+        }
+        
+        return dp[idx][target] = pick + notPick;
+    }
+
+    public static int findWays2(int num[], int tar) {
+        int n = num.length;
+        int[][] dp = new int[n][tar + 1];
+        for (int[] row : dp) {
+            Arrays.fill(row, -1);
+        }
+                return solve(n - 1, tar, num, dp);
     }
 }
